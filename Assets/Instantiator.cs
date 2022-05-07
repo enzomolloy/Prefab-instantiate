69ab88b636c680e3fe6037d669e7101b5a31ba31
enzomolloy <enzomolloy@gmail.com>
2022-05-06 23:43:30 -0300

    Prefab Ex

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(objectToClone);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
